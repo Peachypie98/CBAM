69abfe7d4518b82371a36d2b0c904837ce1731a1
JaeKyu Im <jaekyu.1998.bliz@gmail.com>
2024-01-21 22:07:22 +0900

    bug: fix code on CAM

--- cbam.py
+++ cbam.py
@@ -21,11 +21,7 @@ class CAM(nn.Module):
         super(CAM, self).__init__()
         self.channels = channels
         self.r = r
-        self.linear_max = nn.Sequential(
-            nn.Linear(in_features=self.channels, out_features=self.channels//self.r, bias=True),
-            nn.ReLU(inplace=True),
-            nn.Linear(in_features=self.channels//self.r, out_features=self.channels, bias=True))
-        self.linear_avg = nn.Sequential(
+        self.linear = nn.Sequential(
             nn.Linear(in_features=self.channels, out_features=self.channels//self.r, bias=True),
             nn.ReLU(inplace=True),
             nn.Linear(in_features=self.channels//self.r, out_features=self.channels, bias=True))
@@ -34,8 +30,8 @@ class CAM(nn.Module):
         max = F.adaptive_max_pool2d(x, output_size=1)
         avg = F.adaptive_avg_pool2d(x, output_size=1)
         b, c, _, _ = x.size()
-        linear_max = self.linear_max(max.view(b,c)).view(b, c, 1, 1)
-        linear_avg = self.linear_avg(avg.view(b,c)).view(b, c, 1, 1)
+        linear_max = self.linear(max.view(b,c)).view(b, c, 1, 1)
+        linear_avg = self.linear(avg.view(b,c)).view(b, c, 1, 1)
         output = linear_max + linear_avg
         output = F.sigmoid(output) * x
         return output
